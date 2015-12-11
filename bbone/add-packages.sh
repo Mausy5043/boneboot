73b7149be8acb74cd1e0aa38f2f63cba9171d6bd
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-11 18:12:29 +0100

    gnuplot support added

--- bbone/add-packages.sh
+++ bbone/add-packages.sh
@@ -7,6 +7,6 @@ echo "Installing packages..."
 #probably already present:
 sudo apt-get -yuV install apt-utils bc git htop screen logrotate lsof nano python rsync sudo
 # probably not yet installed
-sudo apt-get -yuV install nfs-common
+sudo apt-get -yuV install nfs-common gnuplot
 echo "Installing additional Python packages..."
-#sudo apt-get -yuV install ...
+sudo apt-get -yuV install python-gnuplot
