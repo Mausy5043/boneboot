4ae0b29b5d8a67a40054932c739859e851e193d5
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-12 10:21:03 +0100

    add settings for main user

--- bbone/mod-files.sh
+++ bbone/mod-files.sh
@@ -13,3 +13,16 @@ sudo locale-gen
 echo "Regenerating SSH-keys..."
 sudo rm /etc/ssh/ssh_host_*
 sudo dpkg-reconfigure openssh-server
+
+###
+# default settings for screen
+echo "shell -\$SHELL" > .screenrc
+echo "defscrollback 10000" >> .screenrc
+echo "hardstatus on" >> .screenrc
+echo "hardstatus alwayslastline" >> .screenrc
+echo "hardstatus string \"%{.bW}%-w%{.rW}%n %t%{-}%+w %=%{..G} %H %l %{..Y} %Y-%m-%d %c \"" >> .screenrc
+
+# ssh
+mkdir -m 0700 -p .ssh
+
+echo "$HOME/bin/chkupdate.sh" >> .profile
