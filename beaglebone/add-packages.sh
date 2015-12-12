cabca36b72364f2d97048db3281ba89ac4898566
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-12 16:13:14 +0100

    add fake-hwclock

--- beaglebone/add-packages.sh
+++ beaglebone/add-packages.sh
@@ -7,7 +7,7 @@ echo "Installing packages..."
 #probably already present:
 sudo apt-get -yuV install apt-utils bc git htop screen logrotate lsof nano python rsync sudo wireless-tools wpasupplicant
 # probably not yet installed
-sudo apt-get -yuV install nfs-common gnuplot
+sudo apt-get -yuV install nfs-common gnuplot fake-hwclock
 sudo apt-get -yuV install debian-keyring debian-archive-keyring
 echo "Installing additional Python packages..."
 sudo apt-get -yuV install python-gnuplot
