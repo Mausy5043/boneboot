55f1391c739ceff16ace265df91ea6b32d1a3755
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-12 14:47:25 +0100

    typo corrected and rsyslog.conf added

--- beaglebone/add-packages.sh
+++ beaglebone/add-packages.sh
@@ -8,8 +8,8 @@ echo "Installing packages..."
 sudo apt-get -yuV install apt-utils bc git htop screen logrotate lsof nano python rsync sudo wireless-tools wpasupplicant
 # probably not yet installed
 sudo apt-get -yuV install nfs-common gnuplot
-sudo apt-get -yuV debian-keyring debian-archive-keyring
+sudo apt-get -yuV install debian-keyring debian-archive-keyring
 echo "Installing additional Python packages..."
 sudo apt-get -yuV install python-gnuplot
 
-sudo apt-get upgrade
+sudo apt-get -yuV upgrade
