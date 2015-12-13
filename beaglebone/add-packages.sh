e1ede4d06a9d7b8c5b093945edd97586a357eeda
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-13 11:46:34 +0100

    extra python packages

--- beaglebone/add-packages.sh
+++ beaglebone/add-packages.sh
@@ -10,7 +10,7 @@ sudo apt-get -yuV install apt-utils bc git htop screen logrotate lsof nano pytho
 sudo apt-get -yuV install nfs-common gnuplot fake-hwclock
 sudo apt-get -yuV install debian-keyring debian-archive-keyring
 echo "Installing additional Python packages..."
-sudo apt-get -yuV install python-gnuplot
+sudo apt-get -yuV install build-essential python-dev python-setuptools python-pip python-smbus python-gnuplot
 
 echo "Starting upgrade..."
 sudo apt-get -yuV upgrade
