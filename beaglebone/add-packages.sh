8c8c500942d6e8bc841408e7bd994e2451d83523
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-04-16 17:18:06 +0200

    20160416T1718

--- beaglebone/add-packages.sh
+++ beaglebone/add-packages.sh
@@ -5,21 +5,18 @@ ME=$(whoami)
 HERE=$(pwd)
 
 echo "Installing packages..."
+apt-get -y autoremove
+apt-get autoclean
 # probably already present:
 apt-get -yuV install apt-utils bc git htop screen logrotate lsof nano python rsync
+# add keyrings if not already present
+apt-get -yuV install debian-keyring debian-archive-keyring
 # wireless-tools wpasupplicant
 # probably not yet installed:
 apt-get -yuV install nfs-common gnuplot-nox tree
-# ntp fake-hwclock 
+# ntp fake-hwclock
 # wavemon
-apt-get -yuV install debian-keyring debian-archive-keyring
 # MySQL support:
 apt-get -yuV install mysql-client python-mysqldb
 echo "Installing additional Python packages..."
 apt-get -yuV install build-essential python-dev python-setuptools python-pip python-smbus python-gnuplot python-openssl
-
-# echo "Starting upgrade..."
-# apt-get -yuV upgrade
-
-sudo apt-get -y autoremove
-sudo apt-get -y autoclean
